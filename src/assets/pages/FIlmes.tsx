5d44bd09f0480dcb1abea27bf0d5b6d4dcbff478
jhonatasmendes <liberalinojhonatas@gmail.com>
2024-05-17 09:45:44 -0300

    adicionando imagem

--- src/assets/pages/FIlmes.tsx
+++ src/assets/pages/FIlmes.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
+
 // componentes
 import NavButton from "../components/NavButton";
+import ExternalLink from "../components/ExternalLink";
+import Product from "../components/Product";
 
 function Filmes() {
     const navigate = useNavigate();
@@ -26,11 +29,26 @@ function Filmes() {
                     <NavButton onClick={handleOnClickFilmes}>Filmes</NavButton>
                     <NavButton onClick={handleOnClickSeries}>Series</NavButton>
                     <NavButton onClick={handleOnClickJogos}>Jogos</NavButton>
+
+                    <ExternalLink href="https://facebook.com/">Facebook</ExternalLink>
+                    <ExternalLink href="https://wa.me/5544988228342">WhatsApp</ExternalLink>
+                    <ExternalLink href="https://www.youtube.com/">YouTube</ExternalLink>
+                    <ExternalLink href="https://github.com/">GitHub</ExternalLink>
                 </div>
                 <div className="content">
                     <div className="filmes">
                         <div className="cabecalho">
-                            <h1>Filmes</h1>
+                           
+                        </div>
+                        <h2>Adicionado Recentemente</h2>
+                        <div className="conteudo">
+                            
+                            <div className="recent">
+                            <Product src="/Herança Roubada.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/TF-Heran%C3%A7a%20roubada.mp4?a=view">Herença Perdida</Product>
+                                <Product src="/Assassino da Rua Das Flores.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202023/%20Assassinos%20da%20Lua%20das%20Flores%20.mp4?a=view">Assassino da Rua Das Flores</Product>
+                                <Product src="/kung-fu-panda4.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/KUNG%20FU%20PANDA%204%20(TF_2024).mp4?a=view">kung-fu-panda4</Product>
+                                <Product src="/Batalha dos Drones.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202023/Batalha%20dos%20Drones.mp4?a=view">Batalha dos Drones</Product>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -20,6 +20,9 @@ function Home() {
     const handleOnClickJogos = () => {
         navigate("/jogos");
     };
+    const handleOnClickRedeSociais = () => {
+        navigate("/RedeSociais");
+    };
 
     return (
         <>
@@ -30,7 +33,7 @@ function Home() {
                     <NavButton onClick={handleOnClickSeries}>Series</NavButton>
                     <NavButton onClick={handleOnClickJogos}>Jogos</NavButton>
 
-                    <ExternalLink href="https://github.com/">Link</ExternalLink>
+                    
                 </div>
                 <div className="content">
                     <div className="home">
@@ -38,13 +41,17 @@ function Home() {
                             <h1>Página Inicial</h1>
                             <h2>Seja bem vindo!</h2>
                         </div>
+                         <h2>Adicionado Recentemente</h2>
                         <div className="conteudo">
-                            <h2>Recentes</h2>
+                            
                             <div className="recent">
                             <Product src="/Herança Roubada.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/TF-Heran%C3%A7a%20roubada.mp4?a=view">Herença Perdida</Product>
                                 <Product src="/Assassino da Rua Das Flores.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202023/%20Assassinos%20da%20Lua%20das%20Flores%20.mp4?a=view">Assassino da Rua Das Flores</Product>
                                 <Product src="/kung-fu-panda4.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/KUNG%20FU%20PANDA%204%20(TF_2024).mp4?a=view">kung-fu-panda4</Product>
                                 <Product src="/Batalha dos Drones.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202023/Batalha%20dos%20Drones.mp4?a=view">Batalha dos Drones</Product>
+                            </div>
+                            <div className="recent">
+                            
                             </div>
                         </div>
                     </div>
