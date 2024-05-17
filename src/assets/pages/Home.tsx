e4197f71ad7e5408414f7fca5a5326fbaa4fe174
JHONATAS BASTOS CAMARGO <jhonatas.7942@aluno.pr.senac.br>
2024-05-17 09:42:34 -0300

    modificado

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -44,7 +44,10 @@ function Home() {
                                 <div className="recent">
                                     <Product src="/filme.jpg" link="#">Águas Selvagens</Product>
                                     <Product src="/filme2.jpg" link="#">Maior que o Mundo</Product>
-                                     
+                                    <Product src="/Herança Roubada.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/TF-Heran%C3%A7a%20roubada.mp4?a=view">Herença Perdida</Product>
+                                    <Product src="/Assassino da Rua Das Flores.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202023/%20Assassinos%20da%20Lua%20das%20Flores%20.mp4?a=view">Assassino da Rua Das Flores</Product>
+                                    <Product src="/kung-fu-panda4.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/KUNG%20FU%20PANDA%204%20(TF_2024).mp4?a=view">kung-fu-panda4</Product>
+                                    <Product src="/Batalha dos Drones.jpg" link="https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202023/Batalha%20dos%20Drones.mp4?a=view">Batalha dos Drones</Product>
                                 </div>
                             </div>
                         </div>
