fe31ae154e5c52c81415ac50cf1d93180d928deb
JHONATAS BASTOS CAMARGO <jhonatas.7942@aluno.pr.senac.br>
2024-05-27 09:56:47 -0300

    modificado

--- src/assets/pages/Reprodutor.tsx
+++ src/assets/pages/Reprodutor.tsx
@@ -4,9 +4,11 @@ function Reprodutor() {
     return (
         <>
             <div style={{background: "url(/background.jpg)", width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <video width="1280px" height="720px" controls style={{backgroundColor: "black"}}>
-                    <source src={filmSrc} type="video/mp4"/>
-                </video>
+                <div style={{background: "rgba(0, 0, 0, 0.5)", backdropFilter: "blur(2px)", width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <video width="1280px" height="720px" controls style={{backgroundColor: "black"}}>
+                        <source src={filmSrc} type="video/mp4"/>
+                    </video>
+                </div>
             </div>
         </>
     );
