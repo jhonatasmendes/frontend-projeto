689a0da80191d8238c16fd7b0f17507d85a4caa8
jhonatasmendes <liberalinojhonatas@gmail.com>
2024-05-27 09:55:59 -0300

    adicionado links em m3u8 na pasta links

--- src/assets/pages/Canais.tsx
+++ src/assets/pages/Canais.tsx
@@ -24,7 +24,8 @@ function Canais() {
                                 <h1>Canais Aberto</h1>
                                 <div className="tv">
                                     <M3U8Player url="https://5cf4a2c2512a2.streamlock.net/8016/8016/playlist.m3u8"></M3U8Player>
-                                    <M3U8Player url="https://5cf4a2c2512a2.streamlock.net/8016/8016/playlist.m3u8"></M3U8Player>
+                                    <M3U8Player url="http://wowza4.catve.com.br:1935/live/livestream/media.m3u8"></M3U8Player>
+                                    
                                 </div>
                                 
                             </div>
