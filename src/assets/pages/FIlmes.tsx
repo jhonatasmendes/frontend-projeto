07ce7cacf01186ba041281e96b72b1b88f8993bf
JHONATAS BASTOS CAMARGO <jhonatas.7942@aluno.pr.senac.br>
2024-06-03 11:17:50 -0300

    modificado

--- src/assets/pages/FIlmes.tsx
+++ src/assets/pages/FIlmes.tsx
@@ -64,7 +64,7 @@ function Filmes() {
     };
 
     const recentFilms: Film[] = [
-        { type: "acao", src: "Duro De Atuar 2.jpg", link: "https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/Duro%20De%20Atuar%202.mp4?a=view", title: "Duro De Atuar 2" },
+        { type: "acao", src: "Duro De Atuar 2.jpg", link: "https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Rec%C3%A9m%20Adicionado/Duro%20De%20Atuar%202.mp4", title: "Duro De Atuar 2" },
         { type: "acao", src: "Atlas.jpg", link: "https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202024/Atlas.mp4", title: "Atlas" },
         { type: "acao", src: "Taro Da Morte.jpg", link: "https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202024/Taro%20Da%20Morte.mp4?a=view", title: "Taro Da Morte" },
         { type: "acao", src: "Uma Prova De Coragem.jpg", link: "https://paineltftv.projetojmmidias.workers.dev/0:/Filmes/Filmes%202024/Uma%20Prova%20De%20Coragem.mp4?a=view", title: "Uma Prova De Coragem" },
